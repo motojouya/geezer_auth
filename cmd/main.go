c170283819c401e8ea4a4bd980298a4460fbab27
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-05-08 00:26:39 +0000

    format

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  fmt.Println("Hello, World!\n")
-  fmt.Println("This is a simple Go program.\n")
-  fmt.Println("It prints a message to the console.\n")
+	fmt.Println("Hello, World!\n")
+	fmt.Println("This is a simple Go program.\n")
+	fmt.Println("It prints a message to the console.\n")
 }
