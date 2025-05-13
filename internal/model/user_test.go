f8a42cbf2eb5926d673a2dd1f94e20692c9f7db7
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-05-13 01:08:07 +0000

    test file

---
+++ internal/model/user_test.go
@@ -0,0 +1,34 @@
+package model_test
+
+import (
+	"time"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"model"
+)
+
+func TestSum(t *testing.T) {
+    actual := sum(3, 5)
+    expected := 8
+
+    assert.Equal(t, expected, actual)
+}
+
+func CreateUser(exposeId string, emailId string, name string, botFlag bool) UnsavedUser {
+	return UnsavedUser{
+		exposeId:      exposeId,
+		exposeEmailId: emailId,
+		name:          name,
+		botFlag:       botFlag,
+	}
+}
+
+func NewUser(userId uint, exposeId string, name string, emailId string, email *string, botFlag bool, registeredDate time.Time, companyRole *CompanyRole) User {
+	return User{
+		userId:         userId,
+		companyRole:    companyRole,
+		email:          email,
+		registeredDate: registeredDate,
+		UnsavedUser:    CreateUser(exposeId, name, emailId, botFlag),
+	}
+}
