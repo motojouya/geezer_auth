84d84fd21ed57fe876bc117bf77d0abd778a6ca9
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-05-16 09:57:16 +0000

    password test add

--- internal/model/password_test.go
+++ internal/model/password_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 )
 
-func TestPassword(t *testing.T) {
+func TestPasswordSuccess(t *testing.T) {
 	var password = "password"
-	var hashed, err = model.GetPassword("password")
+	var hashed, err = model.GetPassword(password)
 	if err != nil {
 		t.Error("Failed to hash password")
 	}
@@ -17,5 +17,19 @@ func TestPassword(t *testing.T) {
 		t.Log("Password verification succeeded")
 	} else {
 		t.Error("Password verification failed")
+	}
+}
+
+func TestPasswordFailure(t *testing.T) {
+	var hashed, err = model.GetPassword("password")
+	if err != nil {
+		t.Error("Failed to hash password")
+	}
+
+	var result = model.VerifyPassword(hashed, "passward")
+	if result {
+		t.Error("Password verification succeeded")
+	} else {
+		t.Log("Password verification failed")
 	}
 }
