ea323f558d9f0be1fd1f847e8563dfda838ae2ac
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-05-17 00:44:35 +0000

    access token working

--- internal/model/accessToken.go
+++ internal/model/accessToken.go
@@ -8,45 +8,95 @@ import (
 // TODO middlewareも作ってしまいたい。イメージを掴んで置く
 
 type AccessToken struct {
-  token string
-  expireDate time.Time
+	Token string
+	ExpireDate time.Time
 }
 
-type CustomClaims struct {
-	// TODO 
+type Company struct {
+	ExposeId string
+	Name string
+	Role string
+}
+
+type User struct {
+	ExposeId string
+	EmailId string
+	Email *string
+	Name string
+	BotFlag bool
+	Company *Company
+}
+
+// TODO expire dateを入れなくて大丈夫？
+type GeezerClaims struct {
+	jwt.RegisteredClaims
+	UserExposeId string
+	UserEmailId string
+	UserEmail *string
+	UserName string
+	BotFlag bool
+	CompanyExposeId *string
+	CompanyName *string
+	CompanyRole *string
+}
+
+type JwtSecret struct {
+	secret []byte
+	version string
 }
 
 // expireの基準日がいるので、日付が必要。オプションで期間の調整ができてもいいかもしれない
-func PublishAccessToken(accessTokenSecret string, user User, tokens []AccessToken, expireDate time.Time) AccessToken {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+func PublishAccessToken(jwtSecret JwtSecret, user User, tokens []AccessToken, expireDate time.Time) (AccessToken, error) {
+
+	var claims = GeezerClaims{
+		Issuer:    "issuer", // iss
+		Subject:   "subject", // sub
+		Audience:  []string{"audience"}, // aud
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(10 + time.Minute)), // exp
+		NotBefore: jwt.NewNumericDate(time.Now()), // nbf
+		IssuedAt:  jwt.NewNumericDate(time.Now()), // iat
+		ID:        "id", // jti
+
 		"foo": "bar",
 		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
-	})
+	}
 
-	tokenString, err := token.SignedString([]byte(accessTokenSecret))
+	var token = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtSecret.secret)
 	if err != nil {
-		// TODO: handle error
-		panic(err)
+        	return "", err
 	}
 
 	return AccessToken{
 		token: tokenString,
 		expireDate: expireDate,
-	}
+	}, nil
 }
 
-func GetUserFromAccessToken(accesstokensecret string, tokenString string) User {
+func GetUserFromAccessToken(jwtSecret JwtSecret, tokenString string) User {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return []byte(accesstokensecret), nil
+		return jwtSecret.secret, nil
 	}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+
+ 	token, err := jwt.ParseWithClaims(
+ 		tokenString,
+ 		&GeezerClaims{},
+ 		func(token *jwt.Token) (interface{}, error) {
+ 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+ 				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+ 			}
+ 			return secret, nil
+ 		},
+ 	)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		return NewUser(claims["foo"])
+	if claims, ok := token.Claims.(GeezerClaims); ok && token.Valid {
+		return NewUser(claims.Issuer)
 	} else {
 		return nil
 	}
