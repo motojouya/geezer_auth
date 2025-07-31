19151991c398e9233f3deb185cd1d6aaa82d4ab5
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-07-31 22:30:21 +0000

    a

--- internal/core/essence/closable.go
+++ internal/core/essence/closable.go
@@ -3,3 +3,15 @@ package essence
 type Closable interface {
 	Close() error
 }
+
+func Close(c interface{}) error {
+	if c == nil {
+		return nil
+	}
+
+	if closer, ok := c.(Closable); ok {
+		return closer.Close()
+	}
+
+	return nil
+}
