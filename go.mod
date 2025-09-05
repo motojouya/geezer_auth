4186bc3dc1cf4deb84f8a24ed8611381c43b8d4d
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-09-05 00:06:15 +0000

    a

--- go.mod
+++ go.mod
@@ -26,7 +26,11 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.13.4 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/sys/user v0.4.0 // indirect
 	github.com/moby/term v0.5.2 // indirect
@@ -38,12 +42,16 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
-	golang.org/x/crypto v0.38.0 // indirect
-	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/entry/entry.go
+++ internal/entry/entry.go
@@ -1,26 +1,60 @@
 package entry
 
 import (
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
+	configBehavior "github.com/motojouya/geezer_auth/internal/behavior/config"
+	localPkg "github.com/motojouya/geezer_auth/internal/local"
+	"github.com/motojouya/geezer_auth/internal/shelter/essence"
+	pkgUser "github.com/motojouya/geezer_auth/pkg/shelter/user"
 )
 
-func createHandle[C any, I any, O any](createControl func () C, handleControl func (C, I, *pkgUser.Authentic) (O, error)) echo.HandlerFunc {
-	return func(c echo.Context) error {
+// TODO
+type RequestHeader struct {
+	token string `header:"bearer"`
+}
 
-		var control = createControl()
+func CreateHandle[C any, I any, O any](createControl func() (C, error), handleControl func(C, I, *pkgUser.Authentic) (O, error)) echo.HandlerFunc {
+	return func(c echo.Context) error {
 
 		var request I
 		if err := c.Bind(&request); err != nil {
 			return err
 		}
 
-		// TODO *pkgUser.Authenticの解決
+		var header RequestHeader
+		if err := c.Bind(&header); err != nil {
+			return err
+		}
 
-		var response, err = handleControl(control, request, c.Get("authentic").(*pkgUser.Authentic))
+		var env = localPkg.CreateEnvironment()
+		jwt, err := configBehavior.NewJwtHandlerGet(env).GetJwtHandler()
 		if err != nil {
 			return err
 		}
 
-		return c.Json(200, response)
+		authentic, err := jwt.Parse(header.token)
+		if err != nil {
+			return err
+		}
+
+		control, err := createControl()
+		if err != nil {
+			return err
+		}
+
+		response, err := handleControl(control, request, authentic)
+		if err != nil {
+			return err
+		}
+
+		var con any = control
+		if closable, ok := con.(essence.Closable); ok {
+			err := closable.Close()
+			if err != nil {
+				return err
+			}
+		}
+
+		return c.JSON(200, response)
 	}
 }
