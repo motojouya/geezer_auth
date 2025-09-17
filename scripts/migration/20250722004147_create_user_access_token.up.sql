5b69a208cfd560b041589120a72c0ede218e5a8e
Takuya Sugiyama @motojouya <motojouya@gmail.com>
2025-09-17 01:45:26 +0000

    fix

--- scripts/migration/20250722004147_create_user_access_token.up.sql
+++ scripts/migration/20250722004147_create_user_access_token.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user_access_token (
   persist_key BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_persist_key BIGINT NOT NULL REFERENCES users(persist_key),
-  access_token VARCHAR(255) NOT NULL,
+  access_token VARCHAR(4096) NOT NULL,
   source_update_date TIMESTAMP WITH TIME ZONE NOT NULL,
   register_date TIMESTAMP WITH TIME ZONE NOT NULL,
   expire_date TIMESTAMP WITH TIME ZONE NOT NULL,
